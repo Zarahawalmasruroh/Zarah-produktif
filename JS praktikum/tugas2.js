f41d03c7820bd27498977825c9c3c7d2696fd5f8
Zarahawalmasruroh <168520656+Zarahawalmasruroh@users.noreply.github.com>
2024-05-14 12:30:11 +0700

    Add files via upload

---
+++ JS praktikum/tugas2.js
@@ -0,0 +1,16 @@
+var x= 15;
+  x+="nicky";
+console.log(x)
+var y="arya";
+  y+= 16;
+console.log(y)
+var q="cahyo";
+  q+= 17;
+console.log(q)
+var t="darun";
+  t+= 16;
+console.log(y)
+var x= 150;
+  x+="galih";
+console.log(x)
+  
